84a6453bf5bad3a62d71533bf7f6e33e183f6d5f
Christopher Mc Laughlin <chrismcl9@hotmail.com>
2018-03-06 15:55:02 +0000

    Finished Exercise 2

--- src/lec4exe2.hs
+++ src/lec4exe2.hs
@@ -5,3 +5,21 @@ data Lit = Lit Integer
 
 instance Eval Lit where
   eval (Lit n) = n
+
+
+data Add x y = Add x y
+
+instance (Eval x, Eval y) => Eval (Add x y) where
+    eval (Add x y) = eval x + eval y
+
+
+data Mul p q = Mul p q
+
+instance (Eval p, Eval q) => Eval (Mul p q) where
+    eval (Mul p q) = eval p * eval q
+
+
+data Subt s t = Subt s t
+
+instance (Eval s, Eval t) => Eval (Subt s t) where
+    eval (Subt s t) = eval s - eval t
